ec2b3e5652feae19f011c151b05680f858266579
tjeaneric <ericjohn415@gmail.com>
2023-03-22 16:18:06 +0200

    Added catching all routes

---
+++ src/components/nav/TheNotFound.vue
@@ -0,0 +1,5 @@
+<template>
+  <h2 style="text-align: center">
+    Page not found, Please visit our <router-link to="/teams">home</router-link> page
+  </h2>
+</template>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import App from './App.vue'
 import TeamsList from './components/teams/TeamsList.vue'
 import UsersList from './components/users/UsersList.vue'
 import TeamMembers from './components/teams/TeamMembers.vue'
+import TheNotFound from './components/nav/TheNotFound.vue'
 
 const routes = [
+  { path: '/', redirect: '/teams' },
   {
     path: '/teams',
     name: 'Teams',
@@ -20,7 +22,8 @@ const routes = [
     path: '/users/',
     name: 'Users',
     component: UsersList
-  }
+  },
+  { path: '/:notFound(.*)*', name: 'notFound', component: TheNotFound }
 ]
 
 const rourer = createRouter({
